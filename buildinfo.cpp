88e32b177aceb2ab3426665e175503ccbbf7cfbc
Akshay Raj Gollahalli <akshay@gollahalli.com>
2026-08-12 14:35:34 +1200

    Properly use Qt and ffmpeg version

---
+++ buildinfo.cpp
@@ -0,0 +1,22 @@
+#include "buildinfo.h"
+
+#ifdef GAV_HAS_FFVERSION_HEADER
+extern "C" {
+#include <libavutil/ffversion.h>
+}
+#endif
+
+BuildInfo::BuildInfo(QObject *parent) : QObject(parent) {
+}
+
+QString BuildInfo::qtVersion() {
+    return QString::fromLatin1(qVersion());
+}
+
+QString BuildInfo::ffmpegVersion() {
+#ifdef FFMPEG_VERSION
+    return QStringLiteral(FFMPEG_VERSION);
+#else
+    return {};
+#endif
+}
